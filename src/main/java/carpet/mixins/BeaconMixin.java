fda156a834fa8bda54ead13e267fa20d4cce8da6
PyRectangle <PyRectangle@web.de>
2021-08-10 17:18:38 +0200

    Dumb autocomplete

--- src/main/java/carpet/mixins/BeaconMixin.java
+++ src/main/java/carpet/mixins/BeaconMixin.java
@@ -12,8 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import sun.awt.image.ImageWatched;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 
