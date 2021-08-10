c1ba7f9a7c9d74ea33fb201c54986bb67c37168f
PyRectangle <PyRectangle@web.de>
2021-08-10 14:51:24 +0200

    Implement Dragon Egg Instant Fall

--- src/main/java/carpet/mixins/FallingBlockMixin.java
+++ src/main/java/carpet/mixins/FallingBlockMixin.java
@@ -42,7 +42,7 @@ public abstract class FallingBlockMixin extends Block
         {
             if (canFallThrough(serverWorld_1.getBlockState(blockPos_1.down(1))) && blockPos_1.getY() >= 0)
             {
-                if (!DragonEggBedrockBreaking.fallInstantly &&
+                if (!(DragonEggBedrockBreaking.fallInstantly || CarpetSettings.dragonEggInstantFall) &&
                         serverWorld_1.getChunkManager().shouldTickChunk(new ChunkPos(blockPos_1)))
                 {
                     if (!serverWorld_1.isClient)
